7a1586f0f2058e4f683c6f0c2fb94851ff2ebb2d
sdsutariya <sutariyasumit8000@gmail.com>
2022-09-13 09:10:18 +0530

    cart related feature

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,14 @@
 import express from "express"
+import verifymiddelware from '../middelware/verifyToken.js';
+import cartcontroller from '../controllers/cartcontrollers.js'
 const router = express.Router();
+
+router.post("/",verifymiddelware.verifyToken, cartcontroller.createCart);
+router.put("/:id",verifymiddelware.verifyTokenAndAuthorization, cartcontroller.updateCart);
+router.delete("/:id",verifymiddelware.verifyTokenAndAuthorization, cartcontroller.deleteCart);
+router.get("/find/:userId",verifymiddelware.verifyTokenAndAuthorization, cartcontroller.getCart);
+router.get("/",verifymiddelware.verifyTokenAndAdmin, cartcontroller.getAllCart);
+
 
 
 export default router
