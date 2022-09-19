9eed4eb8e83699ca5806282401aad5524860af09
Parimal2607 <parimal81280@gmail.com>
2022-09-19 21:55:59 +0530

    push

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import authcontroller from "../controllers/authcontrollers.js";
-const router = express.Router();
-
-//signup
-router.post("/register",authcontroller.register);
-router.post("/login",authcontroller.login);
-
-export default router
--- routes/stripe.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-const router = express.Router();
-import stripecontroller from "../controllers/stripecontrollers.js";
-
-router.post('/payment',stripecontroller.payment)
-
-export default router
